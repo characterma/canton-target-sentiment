c2fa9b3f78534e5bf366746160bc0179f7e0e717
Tony Chan <tonychan@wisers.com>
2020-12-22 02:26:38 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="cantonsa",
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     version="0.1.0",
     description="Catonese target sentiment analysis.",
     author="",
