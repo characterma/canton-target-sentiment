81a2edd286f634ef981a1f58feadc053b475bffb
Tony Chan <tonychan@wisers.com>
2021-01-08 05:32:25 +0000

    add script for locust tests

---
+++ deploy/test/run_all_test.sh
@@ -0,0 +1,6 @@
+python ./locustfile.py -u 50 -r 5 --api $1
+python ./locustfile.py -u 100 -r 10 --api $1
+python ./locustfile.py -u 200 -r 20 --api $1
+python ./locustfile.py -u 300 -r 30 --api $1
+python ./locustfile.py -u 400 -r 40 --api $1
+python ./locustfile.py -u 500 -r 50 --api $1
