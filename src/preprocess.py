6d52c0355c115726d091cb5ff512c1015a1de9ba
Tony Chan <tonychan@wisers.com>
2021-07-21 05:29:02 +0000

    update

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,54 +10,56 @@ FULL2HALF[0x3000] = 0x20
 
 class Preprocessor:
     def __init__(self, data_dict, steps):
-        self.cc = OpenCC('t2s')
+        self.cc = OpenCC("t2s")
         self.data_dict = copy.deepcopy(data_dict)
 
         for s in steps:
             getattr(self, s)()
 
     def rm_non_chinese_char(self):
-        filtrate = re.compile(u'[^\u4E00-\u9FA5]')  
-        self.data_dict['content'] = filtrate.sub(r'', self.data_dict['content'])
+        filtrate = re.compile(u"[^\u4E00-\u9FA5]")
+        self.data_dict["content"] = filtrate.sub(r"", self.data_dict["content"])
 
     def full_to_half(self):
-        self.data_dict['content'] = self.data_dict['content'].translate(FULL2HALF)
+        self.data_dict["content"] = self.data_dict["content"].translate(FULL2HALF)
 
     def utf8_replace(self):
-        self.data_dict['content'] = self.data_dict['content'].encode('utf-8', errors='replace').decode('utf-8')
+        self.data_dict["content"] = (
+            self.data_dict["content"].encode("utf-8", errors="replace").decode("utf-8")
+        )
 
     def simplified_chinese(self):
-        self.data_dict['content'] = self.cc.convert(self.data_dict['content'])
+        self.data_dict["content"] = self.cc.convert(self.data_dict["content"])
 
     def lower_case(self):
-        self.data_dict['content'] = self.data_dict['content'].lower()
+        self.data_dict["content"] = self.data_dict["content"].lower()
 
     def convert_java_index(self):
-        assert("content" in self.data_dict)
-        assert("target_locs" in self.data_dict)
+        assert "content" in self.data_dict
+        assert "target_locs" in self.data_dict
 
         index_map = dict()
         indent = 0
-        for i in range(len(self.data_dict['content']) + 1):
+        for i in range(len(self.data_dict["content"]) + 1):
             for j in range(i, i + indent + 1):
                 if j not in index_map:
                     index_map[j] = i
-            if i < len(self.data_dict['content']):
-                if len(self.data_dict['content'][i].encode('utf-8')) == 4:
+            if i < len(self.data_dict["content"]):
+                if len(self.data_dict["content"][i].encode("utf-8")) == 4:
                     indent += 1
 
-        for t in self.data_dict['target_locs']:
+        for t in self.data_dict["target_locs"]:
             t[0] = index_map.get(t[0], t[0])
             t[1] = index_map.get(t[1], t[1])
 
     def extract_post_context_1(self):
-        assert("content" in self.data_dict)
-        assert("target_locs" in self.data_dict)
+        assert "content" in self.data_dict
+        assert "target_locs" in self.data_dict
         return self._extract_post_context(n_prev=0, n_next=0)
 
     def extract_post_context_2(self):
-        assert("content" in self.data_dict)
-        assert("target_locs" in self.data_dict)
+        assert "content" in self.data_dict
+        assert "target_locs" in self.data_dict
         return self._extract_post_context(n_prev=1, n_next=1)
 
     def _extract_post_context(self, n_prev=0, n_next=0):
@@ -69,46 +71,50 @@ class Preprocessor:
                 cur_end_idx += len(s) + 1
                 if cur_start_idx <= start_idx < cur_end_idx:
                     if start_idx - cur_start_idx + text_len <= len(s):
-                        return i, start_idx - cur_start_idx + 1, start_idx - cur_start_idx + 1 + text_len 
+                        return (
+                            i,
+                            start_idx - cur_start_idx + 1,
+                            start_idx - cur_start_idx + 1 + text_len,
+                        )
                     else:
                         return None, None, None
-                        
+
                 cur_start_idx += len(s) + 1
             return None, None, None
-            
+
         def neighbor_sentences(sentence_id, sents, n_prev=0, n_next=0):
             prev = []
             next = []
             sents_with_id = list(zip(range(len(sents)), sents))
             for s in sents_with_id[:sentence_id][-1::-1]:
-                if s[1].strip()!="":
+                if s[1].strip() != "":
                     prev.append(s[0])
 
-            if sentence_id!=len(sents)-1:
-                for s in sents_with_id[sentence_id+1:]:
-                    if s[1].strip()!="":
+            if sentence_id != len(sents) - 1:
+                for s in sents_with_id[sentence_id + 1 :]:
+                    if s[1].strip() != "":
                         next.append(s[0])
-            
+
             prev = prev[:n_prev][-1::-1]
             next = next[:n_next]
             return prev, next
 
-        sents = re.split("\?|!|。|？|！|\n|\r", self.data_dict['content'])
+        sents = re.split("\?|!|。|？|！|\n|\r", self.data_dict["content"])
         reconst_sent_ids = []
         reconst_target_indices = []
 
         tgt_sent_id_to_neighbor = {}
         tgt_sent_id_to_indices = {}
 
-        for start_idx, end_idx in self.data_dict['target_locs']:
-            
+        for start_idx, end_idx in self.data_dict["target_locs"]:
+
             sentence_id, start_idx, end_idx = find_idices_after_split(
-                start_idx,
-                end_idx,
-                sents
+                start_idx, end_idx, sents
             )
-            
-            prev_sents, next_sents = neighbor_sentences(sentence_id, sents, n_prev=n_prev, n_next=n_next)
+
+            prev_sents, next_sents = neighbor_sentences(
+                sentence_id, sents, n_prev=n_prev, n_next=n_next
+            )
             tgt_sent_id_to_neighbor[sentence_id] = (prev_sents, next_sents)
             tgt_sent_id_to_indices[sentence_id] = (start_idx, end_idx)
 
@@ -126,19 +132,18 @@ class Preprocessor:
                     cur_start_idx += len(sents[idx]) + 1
                     cur_end_idx += len(sents[idx]) + 1
                     cur_len += len(sents[idx]) + 1
-                    
+
             reconst_sent_ids.append(tgt_sent_id)
             cur_len += len(sents[tgt_sent_id]) + 1
-            
+
             for idx in next_sents:
                 if idx not in reconst_sent_ids and idx not in tgt_sent_id_to_indices:
                     reconst_sent_ids.append(idx)
                     cur_len += len(sents[idx]) + 1
 
             reconst_target_indices.append([cur_start_idx, cur_end_idx])
-        
+
         reconst_content = "。".join([sents[i] for i in reconst_sent_ids])
 
-        self.data_dict['content'] = reconst_content
-        self.data_dict['target_locs'] = reconst_target_indices
-            
+        self.data_dict["content"] = reconst_content
+        self.data_dict["target_locs"] = reconst_target_indices
