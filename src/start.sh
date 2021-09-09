667b18496af61b18356601716e285076f393aea8
Tony Chan <tonychan@wisers.com>
2021-09-09 04:47:37 +0000

    test onnx deploy

--- src/start.sh
+++ src/start.sh
@@ -1,1 +1,2 @@
-python serve_fastapi.py --config_dir="../output/org_bert_avg_20210906/model" --device="cpu"
+python serve_fastapi_onnx.py --config_dir="../output/org_bert_avg_20210906/model" --device="cpu"
+# python serve_fastapi_onnx.py --config_dir="../output/org_bert_avg_20210906/model" --device="cpu"
