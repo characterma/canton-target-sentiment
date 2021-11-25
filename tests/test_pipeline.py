35e47941a29e4c6c19131eb8cf106951201b4e40
Tony Chan <tonychan@wisers.com>
2021-11-25 03:00:14 +0000

    update test case

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -38,28 +38,6 @@ output_notebook = "../notebooks/pipeline/train_new_model_with_specific_config_tm
 
 
 class TestPipeline(unittest.TestCase):
-    def test_pipeline_with_default_model(self):
-        model_dir = "../output/test_pipeline_with_default_model_tmp" 
-        for task, config in task_to_config.items():
-            pm.execute_notebook(
-                notebook_path,
-                output_notebook,
-                kernel_name=kernel_name,
-                parameters=dict(
-                    src_dir=src_dir,
-                    task=task, 
-                    device=device, 
-                    model=None,
-                    text_prepro=config['text_prepro'],
-                    model_params=config['model_params'],
-                    train_params=config['train_params'],
-                    eval_params=config['eval_params'],
-                    model_dir=model_dir
-                ),
-            )
-            results = sb.read_notebook(output_notebook).scraps.data_dict
-            self.assertAlmostEqual(results["macro_f1"], target_macro_f1, delta=tolerance)
-
     def test_pipeline_with_specific_model(self):
         model_dir = "../output/test_pipeline_with_specific_model_tmp" 
         for task, config in task_to_config.items():
@@ -81,6 +59,28 @@ class TestPipeline(unittest.TestCase):
             )
             results = sb.read_notebook(output_notebook).scraps.data_dict
             self.assertAlmostEqual(results["macro_f1"], target_macro_f1, delta=tolerance)
+
+    # def test_pipeline_with_default_model(self):
+    #     model_dir = "../output/test_pipeline_with_default_model_tmp" 
+    #     for task, config in task_to_config.items():
+    #         pm.execute_notebook(
+    #             notebook_path,
+    #             output_notebook,
+    #             kernel_name=kernel_name,
+    #             parameters=dict(
+    #                 src_dir=src_dir,
+    #                 task=task, 
+    #                 device=device, 
+    #                 model=None,
+    #                 text_prepro=config['text_prepro'],
+    #                 model_params=config['model_params'],
+    #                 train_params=config['train_params'],
+    #                 eval_params=config['eval_params'],
+    #                 model_dir=model_dir
+    #             ),
+    #         )
+    #         results = sb.read_notebook(output_notebook).scraps.data_dict
+    #         self.assertAlmostEqual(results["macro_f1"], target_macro_f1, delta=tolerance)
 
     def tearDown(self):
         os.system(f"rm {output_notebook}")
