6bb0ec50b7cd7b32c6ea3c8b91c6c356e31e71d7
hinova <hinovawong@wisers.com>
2021-12-10 08:31:42 +0000

    add DTD method in trainer kd

--- src/trainer_kd.py
+++ src/trainer_kd.py
@@ -48,6 +48,47 @@ class KDTrainer(Trainer):
         self.kd_config = args.kd_config
 
     @staticmethod
+    def get_dynamic_temperature(student_logits, teacher_logits, kd_config):
+        '''
+        input:
+        - soft_student_logits: torch.tensor
+        - soft_teacher_logits: torch.tensor
+        - kd_config: dict
+
+        output:
+        - torch.tensor (1 dimension)
+        '''
+        types = kd_config['dtd_type']
+        dtd_bias = kd_config['dtd_bias']
+        dtd_flsw_pow = kd_config['dtd_flsw_pow']
+        initial_temp = kd_config['kl_T']
+
+        if types == 'NA':
+            return torch.tensor(
+                [[initial_temp for i in range(teacher_logits.shape[0])]]
+            ).T.to(student_logits.device)
+
+        wx = []
+        soft_student_logits = softmax(student_logits, dim=-1)
+        soft_teacher_logits = softmax(teacher_logits, dim=-1)
+        for i in range(soft_teacher_logits.shape[0]):
+            if types == 'flsw':
+                # cos = nn.CosineSimilarity(dim = 0, eps=1e-6)
+                wx.append((1- torch.dot(soft_student_logits[i], soft_teacher_logits[i]))**dtd_flsw_pow)
+            elif types == 'cwsm':
+                wx.append(1/torch.max(soft_student_logits[i]))
+            else:
+                raise ValueError(f"Expected knowledge distillation loss type 'NA' or 'flsw' or 'cwsm'")
+
+        kl_temp = []
+        for i in range(soft_teacher_logits.shape[0]):
+            dy_temp = initial_temp + (sum(wx)/soft_teacher_logits.shape[0] - wx[i]) * dtd_bias
+            dy_temp = dy_temp if dy_temp >= 1 else 1
+            kl_temp.append(dy_temp)
+        return torch.tensor([kl_temp]).T.to(student_logits.device)
+
+
+    @staticmethod
     def compute_kd_loss(hard_loss, student_logits, teacher_logits, kd_config):
         if hard_loss is None:
             hard_loss = 0
@@ -63,11 +104,17 @@ class KDTrainer(Trainer):
                 input=student_logits, target=teacher_logits, reduction="mean"
             )
         elif loss_type == "kl":
+            kl_T = KDTrainer.get_dynamic_temperature(student_logits, teacher_logits, kd_config)
             soft_student = log_softmax(student_logits / kl_T, dim=-1)
             soft_teacher = softmax(teacher_logits / kl_T, dim=-1)
-            soft_loss = kl_T ** 2 * kl_div(
-                soft_student, soft_teacher, reduction="batchmean"
-            )
+            # sum of classes KL -> soften logit -> batchmean
+            soft_loss = (kl_T ** 2 * kl_div(
+                soft_student, soft_teacher, reduction="none"
+            ).sum(axis=1)).mean()
+            # old method
+            # soft_loss = kl_T ** 2 * kl_div(
+            #     soft_student, soft_teacher, reduction="batchmean"
+            # )
         else:
             raise ValueError(f"Expected knowledge distillation loss type 'mse' or 'kl'")
         loss = (1 - soft_lambda) * hard_loss + soft_lambda * soft_loss
