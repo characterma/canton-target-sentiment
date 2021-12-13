7ecf3b007612338d878dee2e5bb58960c425db2a
Tony Chan <tonychan@wisers.com>
2021-12-13 03:18:56 +0000

    update streamlit demo

--- demo/app.py
+++ demo/app.py
@@ -1,14 +1,29 @@
 import os
 import json
+import requests
 import streamlit as st
+from streamlit import components
 from pathlib import Path
-os.chdir("../src")
+if os.getcwd().endswith("demo"):
+    os.chdir("../src")
 from pipeline import Pipeline
+from explain.utils import visualize_data_record_bert, get_explanation, get_segment_level_explanation
 
 
 st.set_page_config(
     page_title="NLP Pipeline", layout="wide"
 )
+
+
+def get_word_segments(raw_text):
+
+    data = requests.post("https://ai.wws.wisers.net/ltp-commercial/ltp/article/single/pos", json={'content': raw_text}).json()
+    segments = []
+    segments_idxs = []
+    for sent_res in data['content']['sent_res']:
+        segments += sent_res['words']
+        segments_idxs += sent_res['art_indexs']
+    return segments, segments_idxs
 
 
 def detect_valid_model_dirs():
@@ -43,22 +58,53 @@ def load_model_page():
 
     if pipeline is not None:
         with st.expander("Predict"):
-            content = st.text_input(
-                "content", ""
-            )
+            content = st.text_input("content", "")
 
-            explanation = st.checkbox(
-                "Explanation", 
-                value=False, 
-            )
+            raw_data = {'content': content}
 
-            if st.button("Execute"):
+            if st.button("Predict"):
 
-                prediction = pipeline.predict({
-                    "content": content
-                })
+                prediction = pipeline.predict(raw_data)
+                st.write(prediction)
 
-                prediction
+            if st.button("Interpret"):
+                tokens, scores, attr_target, attr_target_prob, tokens_encoded, faithfulness = get_explanation(
+                    pipeline, 
+                    raw_data, 
+                    enable_faithfulness=True
+                )
+                
+                st.write(f"Faithfulness (COMP) = {faithfulness}")
+                tokens_seg, scores_seg = get_segment_level_explanation(
+                    seg_func=get_word_segments, 
+                    raw_text=raw_data['content'], 
+                    tokens=tokens, 
+                    scores=scores, 
+                    tokens_encoded=tokens_encoded
+                )
+
+                vis = visualize_data_record_bert(
+                    pipeline, 
+                    raw_data, 
+                    tokens=tokens_seg, 
+                    scores=scores_seg, 
+                    attr_target=attr_target, 
+                    attr_target_prob=attr_target_prob
+                )
+
+                # vis = visualize_data_record_bert(
+                #     pipeline, 
+                #     raw_data, 
+                #     tokens=tokens, 
+                #     scores=scores, 
+                #     attr_target=attr_target, 
+                #     attr_target_prob=attr_target_prob
+                # )
+
+                components.v1.html(
+                    vis._repr_html_(), scrolling=True, height=250
+                )
+
 
         with st.expander("Evaluate"):
             uploaded_file = st.file_uploader("Choose a json file")
