0fcb6cfd944c3c566eb890ef1fadf27b2b9a20f2
hinovawong <hinovawong@wisers.com>
2022-11-08 07:16:34 +0000

    unittest rm skipping onnx jit for some tasks

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -24,7 +24,7 @@ class TestEndToEnd(unittest.TestCase):
     skip_onnx = [
                    'chinese_word_segmentation/CNN_CRF',
                    'chinese_word_segmentation/BERT_CRF', 
-                   'target_classification/TGSAN',
+                   # 'target_classification/TGSAN',
                    'target_classification/TGSAN2'
                    'topic_classification/BERT_CLS',
                    'topic_classification/BERT_AVG',
@@ -32,8 +32,8 @@ class TestEndToEnd(unittest.TestCase):
     skip_jit = [
                    'chinese_word_segmentation/CNN_CRF',
                    'chinese_word_segmentation/BERT_CRF', 
-                   'target_classification/TGSAN',
-                   'target_classification/TGSAN2'
+                   # 'target_classification/TGSAN',
+                   # 'target_classification/TGSAN2'
                    'topic_classification/BERT_CLS',
                    'topic_classification/BERT_AVG',
     ]
